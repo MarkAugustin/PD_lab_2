a42deb9bf6562c3de7b60a47a30af29b8fec8efa
MarkAugustin <marc-augustin.stepanenco@isa.utm.md>
2023-10-14 14:09:59 +0300

    making a universal function for roses and tulips

--- lab2.py
+++ lab2.py
@@ -6,47 +6,51 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 
-query = "rose"
-num_images = 1000
-folder = "dataset/rose"
+def DownloadImageFromYandex(query, folder, num_images):
+    os.makedirs(folder, exist_ok=True)
 
-os.makedirs(folder, exist_ok=True)
-
-driver = webdriver.Chrome()
-driver.get(f"https://yandex.ru/images/search?text={query}")
-time.sleep(5)
-scroll_count = 0
+    driver = webdriver.Chrome()
+    driver.get(f"https://yandex.ru/images/search?text={query}")
+    time.sleep(5)
+    scroll_count = 0
 
 
-while scroll_count < num_images // 25:
-    element = driver.find_element(By.TAG_NAME, 'body')
-    element.send_keys(Keys.END)
-    time.sleep(1)
-    scroll_count += 1
-    try:
-        button = driver.find_element(By.CLASS_NAME, 'button2')
-        button.click()
-    except:
-        pass
+    while scroll_count < num_images // 25:
+        element = driver.find_element(By.TAG_NAME, 'body')
+        element.send_keys(Keys.END)
+        time.sleep(1)
+        scroll_count += 1
+        try:
+            button = driver.find_element(By.CLASS_NAME, 'button2')
+            button.click()
+        except:
+            pass
 
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
 
-thumbnails = soup.find_all("img", class_="serp-item__thumb")
-count = 0
+    thumbnails = soup.find_all("img", class_="serp-item__thumb")
+    count = 0
 
-for thumbnail in thumbnails[:num_images]:
-    try:
-        full_image_url = thumbnail["src"]
+    for thumbnail in thumbnails[:num_images]:
+        try:
+            full_image_url = thumbnail["src"]
 
-        filename = f"{str(count).zfill(4)}.jpg"
-        urlretrieve("https:" + full_image_url, os.path.join(folder, filename))
+            filename = f"{str(count).zfill(4)}.jpg"
+            urlretrieve("https:" + full_image_url, os.path.join(folder, filename))
 
-        count += 1
+            count += 1
 
-        if count >= num_images:
-            break
-    except Exception as e:
-        print(f"Ошибка при загрузке изображения: {e}")
+            if count >= num_images:
+                break
+        except Exception as e:
+            print(f"Ошибка при загрузке изображения: {e}")
 
-driver.quit()
+    driver.quit()
+
+
+
+
+DownloadImageFromYandex("rose", "dataset/rose", 1000)
+
+DownloadImageFromYandex("tulip", "dataset/tulip", 1000)
